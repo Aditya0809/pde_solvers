4bf310bb8bb351bc094f71c822ff55b760dca8f2
Aditya0809 <ashankar541@gmail.com>
2026-02-17 18:20:15 -0500

    dummy

---
+++ fd/poisson/poisson_main.cpp
@@ -0,0 +1,15 @@
+#include <mpi.h>
+#include <cstdio>
+
+int main(int argc, char** argv) {
+  MPI_Init(&argc, &argv);
+
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  std::printf("poisson_fd built with MPI. ranks=%d/size=%d\n", rank, size);
+
+  MPI_Finalize();
+  return 0;
+}
